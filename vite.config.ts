2217de1fd6c328b770ba63a2902db37f2550e9b7
mirai-academy <info@frogs-corp.jp>
2026-03-01 06:48:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig} from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    // GitHub PagesのURL（https://mirai-academy.github.io/relogic/）に合わせて設定
-    base: '/relogic/', 
-    plugins: [react(), tailwindcss()],
-    define: {
-      // client-sideで使えるように定義
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
+export default defineConfig({
+  // リポジトリ名に合わせて設定。末尾のスラッシュを忘れずに
+  base: '/relogic/', 
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-    // ビルド時のエラーを防ぐための設定
-    build: {
-      outDir: 'dist',
-    }
-  };
+  },
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+  },
 });
